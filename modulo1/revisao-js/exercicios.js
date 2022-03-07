8d6213a9d845e431ee22f88542d53249d2932fcb
Diego Lima <deduardo.lm@gmail.com>
2022-03-07 16:50:29 -0300

    inclusão-revisaojs

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,49 +5,82 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-    return array.length
+  //  return array.length
 
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-    return array.reverse()
+  //  return array.reverse()
   
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+ //   return array.sort(function(a, b) {
+ //       return a - b
+ //   })
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+  return array.filter((item) =>{
+      if ( item % 2 === 0){
+          return true
+      }else {
+          return false}
+
+  })  
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+   return array.filter((item) =>{
+        if (item % 2 === 0){
+            return true 
+        }else {
+            return false}
+  
+    })
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  let maior = 0
+  for(let num of array){
+      if (num > maior) {
+          maior = num
+      
+        }
+  } return maior 
+
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
 
-}
+
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
-}
+   let arrayPares = []
+    for(let numero = 0; arrayPares.length < n ; numero++ )
+            if ( number % 2 === 0){
+                arrayPares.push(numero)
+            }
+            return arrayPares
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+  if (ladoA&&ladoB&&ladoC){
+      return `Equilátero`
+  } 
+  else if (ladoA||ladoB||ladoC){
+    return `Isósceles` }
+ 
+    else if (ladoA!==ladoB!==ladoC){
+         return `Escaleno`
+ }
 }
 
 // EXERCÍCIO 10
