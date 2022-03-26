ca4cb7d7dcd706024218b0ecee5f281bb8a09804
Diego Lima <deduardo.lm@gmail.com>
2022-03-25 22:19:26 -0300

    Projeto Whatslab

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,17 +1,71 @@
 import React from "react";
+import styled, {createGlobalStyle} from "styled-component";
+
+const GlobalStyled = createGlobalStyle`
+*{
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+}`
 
 
-class App extends React.Component {
-  render(){
-
-
-
-
-
-
-
-    
-}
+export default class App extends React.Component {
+  state = { 
+    whatslab:[
+    { usuario: "", mensagem: ""}
+  ],
+  inputUsuario:"",
+  inputMensagem: ""
 };
 
-export default App;
+onChangeUsuario = (event) => {
+  this.setState({
+    inputUsuario: event.target.value
+  });
+}
+onChangeMensagem = (event) => {
+    this.setState({
+      inputMensagem: event.target.value
+  });
+}
+adicionarMensagem = () => {
+  const novasMensagens = [...this.state.whatslab];
+  const novaMensagem = {
+    usuario: this.state.inputUsuario,
+    mensagem: this.state.inputMensagem};
+  novasMensagens.push(novaMensagem);
+    
+    this.setState({
+      whatslab:novasMensagens,
+      inputUsuario: "",
+      inputMensagem: ""
+    });
+  }
+render() {
+  const componentesMensagem = this.state.whatslab.map((whatslab) => {
+    return (
+      <CardMensagem key={whatslab.usuario}>
+        <p>{whatslab.usuario}</p>:<p>{whatslab.mensagem}</p>
+      </CardMensagem>
+    );
+    });   
+  return (
+    <>
+    <GlobalStyled/>
+    <input
+      placeholder="Usuário"
+      value={this.state.inputUsuario}
+      onChange={this.onChangeUsuario}
+    />
+    <input
+      placeholder="Mensagem"
+      value={this.state.inputMensagem}
+      onChange={this.onChangeMensagem}
+      type="text"
+    />
+    <button onClick={this.adicionarMensagem}>Adicionar</button>
+    {componentesMensagem}
+    </>
+    );
+  }
+}    
