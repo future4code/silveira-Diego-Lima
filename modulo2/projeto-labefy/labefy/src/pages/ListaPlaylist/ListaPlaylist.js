9dbcc9946c3590b748c9f214bcc19ef3b6194973
Diego Lima <deduardo.lm@gmail.com>
2022-04-08 17:55:53 -0300

    inclusão labefy

--- modulo2/projeto-labefy/labefy/src/pages/ListaPlaylist/ListaPlaylist.js
+++ modulo2/projeto-labefy/labefy/src/pages/ListaPlaylist/ListaPlaylist.js
@@ -1,28 +1,79 @@
 import React from "react";
 import axios from "axios";
-import {BASE_URL} from '../../constants/urls'
+import { BASE_URL } from '../../constants/urls'
 
 const headers = {
-    headers 
+        headers: {
+        Authorization: "diego-lima-silveira"
+    }
 }
 
+const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
 export default class ListaPlaylist extends React.Component {
     state = {
-        listas: []
+        lista: [],
+        inputNome:""
     }
 
-
-
+    componentDidMount() {
+        
+    }
     getPlaylists = () => {
-        axios.get({BASE_URL})
-
-
+        axios.get(url, headers)
+            .then((res) =>
+                console.log(res.list))
+            .catch((err) =>
+                console.log(err.response))
     }
 
-    render(){ 
-        return (
 
-            <div>Lista</div>
+    criarPlaylist = () =>{
+
+        const body = {
+            name: this.state.inputNome
+        }
+       
+        axios.post(url, body, headers)
+            .then((res) =>
+                console.log(res))
+            .catch((err) =>
+                console.log(err.response))
+    }
+
+
+    onChangeInputNome = (e) => {
+        this.setState({inputNome: e.target.value})
+
+    } 
+
+    
+
+    render() {
+
+        // const listPlaylist = this.state.lista.map((playlist) => {
+        //     return 
+        //         <div key={playlist.id}>
+        //             {playlist.nome}
+        //             <button onClick={() => this.deletarUsuario(playlist.id)}>X</button>
+
+        //         </div>;
+
+        // });
+
+            return (
+
+            <div> 
+             <input placeholder={"Nome da Playlist"}
+                    value={this.state.inputNome}
+                    onChange={this.onChangeInputNome}
+             /> 
+             
+             
+             
+             <button onClick={this.criarPlaylist}>Criar Playlist</button>     
+                
+                
+            </div>
 
         )
 
