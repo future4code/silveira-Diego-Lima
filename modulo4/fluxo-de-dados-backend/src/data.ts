26fe03e356cced25a5cb8a2680616501b72eca85
Diego Lima <deduardo.lm@gmail.com>
2022-06-01 18:03:22 -0300

    inclusão fuxo de dados

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,21 @@
+
+export type Produtos = {
+  id: string,
+  name: string,
+  price: number
+}
+
+export const produtos: Produtos[] = [
+  {
+  id: "1",
+  name: "camiseta",
+  price: 50
+},{
+  id: "2",
+  name: "agasalho",
+  price: 80
+},{
+  id: "3",
+  name: "calça jeans",
+  price: 70
+}];
