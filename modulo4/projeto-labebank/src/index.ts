faf91a75b44482791be83677778b8ad9495d8fcb
Diego Lima <deduardo.lm@gmail.com>
2022-06-03 17:31:18 -0300

    inclusão projeto labebank

---
+++ modulo4/projeto-labebank/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// Para testar se o servidor está tratando os endpoints corretamente
+app.get("/ping", (req: Request, res: Response) => {
+  res.status(200).send("pong!")
+})
+
+app.listen(3003, () => {
+  console.log('Server is running at port 3003')
+})
