a5f5729d19715d3e96f8901d89e8fdaee1163642
Diego Lima <deduardo.lm@gmail.com>
2022-06-04 21:22:02 -0300

    alteração

--- modulo4/projeto-labebank/src/data.ts
+++ modulo4/projeto-labebank/src/data.ts
@@ -1,57 +1,16 @@
-type User = {
-    id: number,
-    name: string,
-    email: string,
-    type: UserType,
-    age: number
+
+export type Trasacoes = {
+    valor: number,
+    data: Date,
+    descricao: string
+}
+export type User = {
+    nome: string,
+    CPF: string,
+    dataNascimento: Date,
+    saldo: number
+    extrato : Trasacoes[]
   }
-  enum UserType {
-    ADMIN = "ADMIN",
-    NORMAL = "NORMAL"
-  }
-  
+
   // Mock simulando um array de usuários no Banco de Dados
- export let users: User[] = [
-    {
-        id: 1,
-        name: "Alice",
-        email: "alice@email.com",
-        type: UserType.ADMIN,
-        age: 12
-    },
-    {
-        id: 2,
-        name: "Bob",
-        email: "bob@email.com",
-        type: UserType.NORMAL,
-        age: 36
-    },
-    {
-        id: 3,
-        name: "Coragem",
-        email: "coragem@email.com",
-        type: UserType.NORMAL,
-        age: 21
-    },
-    {
-        id: 4,
-        name: "Dory",
-        email: "dory@email.com",
-        type: UserType.NORMAL,
-        age: 17
-    },
-    {
-        id: 5,
-        name: "Elsa",
-        email: "elsa@email.com",
-        type: UserType.ADMIN,
-        age: 17
-    },
-    {
-        id: 6,
-        name: "Fred",
-        email: "fred@email.com",
-        type: UserType.ADMIN,
-        age: 60
-    }
-  ]
+ export let users: User[] = []
--- modulo4/projeto-labebank/src/index.ts
+++ modulo4/projeto-labebank/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { User, users } from './data'
 
 const app = express()
 app.use(express.json())
@@ -13,3 +14,51 @@ app.get("/ping", (req: Request, res: Response) => {
 app.listen(3003, () => {
   console.log('Server is running at port 3003')
 })
+
+app.post("/users", (req: Request, res: Response) => {
+  let codeError = 400
+  try {
+    const { nome, CPF, dataNascimentoString} = req.body
+    if( !nome || !CPF || !dataNascimentoString){
+      codeError = 404
+      throw new Error (" O nome , cpf ou data de nascimento são necessarios")
+    }
+
+    const [dia, mes, ano] = dataNascimentoString.split("/")
+    const dataNascimento: Date = new Date(`${ano}-${mes}-${dia}`)
+    const idadeEmMilisegundos: number = Date.now() - dataNascimento.getTime()
+    const idadeEmAnos: number = idadeEmMilisegundos /1000 /60 /60 /24 /365  
+
+    if(idadeEmAnos < 18){
+      codeError = 406
+      throw new Error("Idade deve ser maior que 18 anos");      
+    }
+  
+    const newUser:User = {
+      nome,
+      CPF,
+      dataNascimento: dataNascimento,
+      saldo: 0,
+      extrato : [],  
+    }
+    users.push(newUser)
+    res.status(201).send(users)        
+  } catch (error: any) {
+    res.status(codeError).send(error.message)   
+    
+  }
+})  
+
+app.get("/users", (req: Request, res: Response) => {
+  let codeError = 400 
+  try {
+    if(!users.length){
+      codeError = 404
+      throw new Error("Nenhum usuario foi encontrado")
+    }
+    
+    res.status(200).send(users)
+  } catch (error: any) {
+    res.status(codeError).send(error.message)
+  }
+})  
