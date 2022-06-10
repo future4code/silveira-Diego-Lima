e2d934393cdd34285fa0ac5cb420446c6a56d777
Diego Eduardo Lima <deduardolima>
2022-06-10 16:56:46 -0300

    inclusção to do list

--- modulo4/to-do-list/src/index.ts
+++ modulo4/to-do-list/src/index.ts
@@ -1,6 +1,8 @@
-import express, {Express} from 'express'
+import express, {Request, Response, Express} from 'express'
 import cors from 'cors'
 import { AddressInfo } from "net";
+import { createUser } from './endpoints/createUser';
+
 
 const app: Express = express();
 
@@ -17,3 +19,20 @@ const server = app.listen(process.env.PORT || 3003, () => {
        console.error(`Failure upon starting server.`);
     }
 });
+
+//Exercico 1
+//Endpoint de criação de usuários
+
+app.post("/user", async (req: Request , res: Response) => {
+    try {
+      await createUser(
+        req.body.name,
+        req.body.nickName,
+        req.body.email
+      );
+      res.status(201).send()      
+    } catch (error:any) {
+      res.status(400).send({ message: error.message || error.sqlMessage })
+    }   
+  });
+
