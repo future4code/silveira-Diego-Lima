8a16c452d2e7c47e18c86df49e06469dbaaa5998
Diego Eduardo Lima <deduardolima>
2022-06-12 18:12:54 -0300

    alteração

--- modulo4/to-do-list/src/endpoints/getusers.ts
+++ modulo4/to-do-list/src/endpoints/getusers.ts
@@ -1,3 +1,7 @@
 import { connection } from "../data/baseDataBase";
 
 
+let dataBrasileira:string = "06/09/1991";
+let dataAmericana:string = dataBrasileira.split('/').reverse().join('-');
+
+console.log(dataAmericana) 
