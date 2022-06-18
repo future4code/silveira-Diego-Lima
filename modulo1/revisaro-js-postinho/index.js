3177f04fc37addcdbf3f2a23d7d7dd90904c1ee2
Diego Eduardo Lima <deduardolima>
2022-06-18 20:56:44 -0300

    alteracao

---
+++ modulo1/revisaro-js-postinho/index.js
@@ -0,0 +1,15 @@
+function find_max(nums) {
+    let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+    for (let num of nums) {
+        if (num > max_num) {
+            // (Fill in the missing line here)
+        }
+    }
+    return max_num;
+}
+
+find_max(nums)
+
+nums = [1, 2, 3, 4, 6, 5, 8, 125, 21515, 1512]
+
+console.log(find_max)
